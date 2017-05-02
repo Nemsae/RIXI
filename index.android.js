b3e35bd5b7294c6e97885bdfb2b13a41abc232c5
John Son <jsson77@gmail.com>
2017-05-01 21:56:33 -0700

    reducer setup

--- index.android.js
+++ index.android.js
@@ -1,55 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
-export default class receiptLogger extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          RIZI: John's Receipt Logger
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import { Provider } from 'react-redux';
+import configureStore from './app/configureStore';
+import App from './app/app.js'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: '#FFEFEE',
-    // backgroundColor: '#FFF6EE',
-    backgroundColor: '#BDCACA',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#EFF5E5',
-    marginBottom: 5,
-  },
-});
+const store = configureStore()
 
-AppRegistry.registerComponent('receiptLogger', () => receiptLogger);
+const Rixi = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent('redux4ways', () => Rixi)
