df3d36cb387ee0e4c0e903c606cb82bfc605bb0b
John Son <jsson77@gmail.com>
2017-05-04 20:11:47 -0700

    homepage index and styles.js init

---
+++ app/components/HomePage/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { TouchableHighlight, Text, View } from 'react-native'
+
+import { connect } from 'react-redux'
+// import { fetchData } from '../../actions/dataActions'
+
+import styles from './styles'
+
+const SplashPage = (props) => {
+  const {
+    container,
+    welcomeText,
+    button,
+    buttonText,
+  } = styles
+
+  return (
+    <View style={container}>
+      <Text style={welcomeText}>Choose Between</Text>
+      <TouchableHighlight style={button} onPress={() => props.fetchData()}>
+        <Text style={buttonText}>CAMERA</Text>
+      </TouchableHighlight>
+      <Text style={welcomeText}>OR</Text>
+      <TouchableHighlight style={button} onPress={() => props.fetchData()}>
+        <Text style={buttonText}>URL</Text>
+      </TouchableHighlight>
+    </View>
+  )
+}
+
+function mapStateToProps (state) {
+  return {
+    appData: state.appData,
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchData: () => dispatch(fetchData()),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SplashPage)
