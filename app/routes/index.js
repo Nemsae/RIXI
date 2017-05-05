19bebc75d50cd2c0baebf91bc8f46db3cb12a95e
John Son <jsson77@gmail.com>
2017-05-04 22:10:02 -0700

    router index init

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,2 +1,18 @@
 import React, { Component } from 'react'
 import { Router, Scene } from 'react-native-router-flux'
+
+import HomePage from '../components/HomePage'
+import SplashPage from '../components/SplashPage'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="splashPage" component={SplashPage} title="Welcome to Rixi" initial={true} />
+          <Scene key="homePage" component={HomePage} title="Rixi Home" />
+        </Scene>
+      </Router>
+    )
+  }
+}
