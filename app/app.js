d6647f341ab7d571896c83a3512a764f7bfd2b53
John Son <jsson77@gmail.com>
2017-05-06 11:19:41 -0700

    app.js update

---
+++ app/app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux'
+
+import SplashPage from './components/SplashPage'
+import HomePage from './components/HomePage'
+import configureStore from './configureStore';
+
+const store = configureStore()
+
+class WelcomeScreen extends React.Component {
+  // static navigationOptions = {
+  //   title: 'Welcome to Rixi',
+  // };
+
+  render() {
+    // console.log('props in App.js: ', this.props);
+    return (
+      <Provider store={store}>
+        <SplashPage navigation={this.props.navigation} />
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+const Rixi = StackNavigator({
+  Splash: { screen: WelcomeScreen },
+  Home: { screen: HomePage }
+});
+
+AppRegistry.registerComponent('receiptLogger', () => Rixi);
