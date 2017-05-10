c4e0bd15c0894877eedc76375c2e972bbed522cf
John Son <jsson77@gmail.com>
2017-05-09 23:52:27 -0700

    subreddit reducer set up

--- app/actions/redditActions.js
+++ app/actions/redditActions.js
@@ -1,5 +1,6 @@
 //  USER INTERACTIONS
 //  USER INTERACTIONS
+//  USER INTERACTIONS
 
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 
@@ -10,9 +11,8 @@ export function selectSubreddit(subreddit) {
   }
 }
 
-//  To Refresh a subreddit
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
-
+//  To Refresh a subreddit
 export function invalidateSubreddit(subreddit) {
   return {
     type: INVALIDATE_SUBREDDIT,
@@ -22,12 +22,28 @@ export function invalidateSubreddit(subreddit) {
 
 //  NETWORK REQUESTS
 //  NETWORK REQUESTS
+//  NETWORK REQUESTS
 
 export const REQUEST_POSTS = 'REQUEST_POSTS'
-
+//  fetching posts for a subreddit
 function requestPosts(subreddit) {
   return {
     type: REQUEST_POSTS,
     subreddit
   }
 }
+
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+//  when network request comes in, dispatch this action
+function receivePosts(subreddit, json) {
+  return {
+    type: RECEIVE_POSTS,
+    subreddit,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+  }
+}
+
+//  THUNK ACTION CREATOR
+//  THUNK ACTION CREATOR
+//  THUNK ACTION CREATOR
